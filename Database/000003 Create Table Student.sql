edc3d27bd6acba405691fa7521fc1f4f195f8457
Nithya Natarajan <nithyanatarajn@gmail.com>
2013-08-28 23:10:34 +0530

    Added student table

---
+++ Database/000003 Create Table Student.sql
@@ -0,0 +1,22 @@
+USE [SMS]
+GO
+
+CREATE TABLE [Core].[Student](
+	[Id] [int] NOT NULL IDENTITY (1,1),
+	[RollNumber] [nvarchar](50) NOT NULL,	
+	[FirstName] [nvarchar](50) NOT NULL,
+	[MiddleName] [nvarchar](50) NULL,
+	[LastName] [nvarchar](50) NOT NULL,
+	[JoinDate] Datetime
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [Core].[Student] ADD CONSTRAINT PK_Student_Id PRIMARY KEY (Id)
+GO
+
+ALTER TABLE [Core].[Student] ADD CONSTRAINT UK_Student_RollNumber UNIQUE (RollNumber)
+GO
+
+ALTER TABLE [Core].[Student] ADD CONSTRAINT UK_Student_JoinDate DEFAULT GETDATE() FOR [JoinDate]
+GO
