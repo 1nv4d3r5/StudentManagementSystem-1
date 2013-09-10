6a11c97c8ba99e08bdc32c83ad3eb59a87735bf4
Nithya Natarajan <nithyanatarajn@gmail.com>
2013-09-11 00:26:19 +0530

    Added a nhibernate mapping for student domain

---
+++ Source/Data.NHibernate/Maps/StudentMap.cs
@@ -0,0 +1,21 @@
+﻿namespace Data.NHibernate.Maps
+{
+    using Domain;
+
+    using FluentNHibernate.Mapping;
+
+    public class StudentMap : ClassMap<Student>
+    {
+        public StudentMap()
+        {
+            this.Schema("Core");
+            this.Table("Student");
+            this.Id(student => student.Id);
+            this.Map(student => student.RollNumber);
+            this.Map(student => student.FirstName);
+            this.Map(student => student.MiddleName);
+            this.Map(student => student.LastName);
+            this.Map(student => student.JoinDate);
+        }
+    }
+}
