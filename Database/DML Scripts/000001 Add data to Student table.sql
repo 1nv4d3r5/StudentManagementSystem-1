a6ae606faa6a916cd07afd5401056739484d6e59
Nithya Natarajan <nithyanatarajn@gmail.com>
2013-09-11 00:04:31 +0530

    Added a new script with student data

---
+++ Database/DML Scripts/000001 Add data to Student table.sql
@@ -0,0 +1,12 @@
+USE [SMS]
+GO
+
+INSERT INTO [Core].[Student]([RollNumber],[FirstName],[LastName]) VALUES (1, 'Nithya', 'Natarajan')
+INSERT INTO [Core].[Student]([RollNumber],[FirstName],[LastName]) VALUES (2, 'Vinodini', 'Senthilnathan')
+INSERT INTO [Core].[Student]([RollNumber],[FirstName],[LastName]) VALUES (3, 'Vidhya', 'Raja')
+INSERT INTO [Core].[Student]([RollNumber],[FirstName],[LastName]) VALUES (4, 'Nivetha', 'Kulashekaran')
+INSERT INTO [Core].[Student]([RollNumber],[FirstName],[LastName]) VALUES (5, 'Arun', 'Pragash')
+INSERT INTO [Core].[Student]([RollNumber],[FirstName],[LastName]) VALUES (6, 'Rahul', 'Raghavendhra')
+
+GO
+
