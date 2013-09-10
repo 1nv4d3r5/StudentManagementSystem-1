a42a065276636ae59aa2baf03c83a2f50068126d
Nithya Natarajan <nithyanatarajn@gmail.com>
2013-09-11 00:13:37 +0530

    Added a student domain object

---
+++ Source/Domain/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain
+{
+    using System;
+
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string RollNumber { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime JoinDate { get; set; }
+    }
+}
