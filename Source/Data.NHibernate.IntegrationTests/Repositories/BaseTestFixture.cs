c78099d47d4ee4b25a4ddcbd2f25c4f13b5f12b7
Nithya Natarajan <nithyanatarajn@gmail.com>
2013-09-15 23:22:20 +0530

    Added a new class to rollback the transaction in tests

---
+++ Source/Data.NHibernate.IntegrationTests/Repositories/BaseTestFixture.cs
@@ -0,0 +1,53 @@
+﻿namespace Data.NHibernate.IntegrationTests.Repositories
+{
+    using System;
+    using System.Transactions;
+
+    using NHibernate.Repositories;
+
+    using NUnit.Framework;
+
+    using global::NHibernate;
+
+    public class BaseTestFixture
+    {
+        protected ISession Session;
+
+        protected ISessionFactory SessionFactory;
+
+        private TransactionScope transactionScope;
+
+        [SetUp]
+        public void SetUpTransactions()
+        {
+            this.transactionScope = new TransactionScope(TransactionScopeOption.RequiresNew, new TimeSpan(0, 0, 1, 0));
+            Session = SessionHelper.GetSession();
+        }
+
+        [TearDown]
+        public void TearDownTransactions()
+        {
+
+            var session = SessionHelper.GetSession();
+
+            if (session.Transaction.IsActive)
+            {
+                session.Transaction.Rollback();
+            }
+            if (session.IsOpen)
+            {
+                session.Clear();
+                session.Close();
+            }
+
+            this.transactionScope.Dispose();
+        }
+
+        protected void FlushAndClearSession()
+        {
+            var session = SessionHelper.GetSession();
+            session.Flush();
+            session.Clear();
+        }
+    }
+}
