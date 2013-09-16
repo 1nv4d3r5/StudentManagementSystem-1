bb5c163c540eebce66ef429d36a0beb2e1f63a53
Nithya Natarajan <nnithya@thoughtworks.com>
2013-09-17 01:11:39 +0530

    Added a table-department and added data for the table

---
+++ Database/DDL Scripts/000004 Create Table Department.sql
@@ -0,0 +1,19 @@
+USE [SMS]
+GO
+
+CREATE TABLE [Core].[Department](
+	[Id] [int] NOT NULL IDENTITY (1,1),
+	[Code] [nvarchar](20) NOT NULL,
+	[Name] [nvarchar](200) NOT NULL,
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [Core].[Department] ADD CONSTRAINT PK_Department_Id PRIMARY KEY (Id)
+GO
+
+ALTER TABLE [Core].[Department] ADD CONSTRAINT UK_Department_Code UNIQUE (Code)
+GO
+
+ALTER TABLE [Core].[Department] ADD CONSTRAINT UK_Department_Name UNIQUE (Name)
+GO
---
+++ Database/DML Scripts/000002 Add data to Department.sql
@@ -0,0 +1,12 @@
+USE [SMS]
+GO
+
+INSERT INTO [Core].[Department]([Code],[Name]) VALUES ('ECE', 'Electronics And Communications Engineering')
+INSERT INTO [Core].[Department]([Code],[Name]) VALUES ('CSC', 'Computer Science Engineering')
+INSERT INTO [Core].[Department]([Code],[Name]) VALUES ('EEE', 'Electrical and Electronics Engineering')
+INSERT INTO [Core].[Department]([Code],[Name]) VALUES ('MECH', 'Mechanical Engineering')
+INSERT INTO [Core].[Department]([Code],[Name]) VALUES ('CIVIL', 'Civil Engineering')
+INSERT INTO [Core].[Department]([Code],[Name]) VALUES ('IT', 'Information Technology')
+
+GO
+
